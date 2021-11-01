4de0fccc58005fa3b11cdfbec70ca5f91d5fc801
softfietea <68200121+softfietea@users.noreply.github.com>
2021-11-01 19:38:47 +0800

    update Map

--- lib/map_new.dart
+++ lib/map_new.dart
@@ -3,8 +3,9 @@ import 'dart:collection';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart' as fmap;
-
 import 'package:latlong2/latlong.dart' as lt;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sylviapp_admin/providers/providers.dart';
 
 class MapCampaignRequest extends StatefulWidget {
   const MapCampaignRequest({Key? key}) : super(key: key);
@@ -53,6 +54,7 @@ class _MapCampaignRequestState extends State<MapCampaignRequest> {
                       center: _initialCameraPosition,
                       zoom: 13,
                     ),
+                    layers: [],
                     children: [
                       fmap.TileLayerWidget(
                         options: fmap.TileLayerOptions(
@@ -68,10 +70,6 @@ class _MapCampaignRequestState extends State<MapCampaignRequest> {
                         fmap.CircleLayerWidget(
                           options: fmap.CircleLayerOptions(
                             circles: [
-                              GestureDetector(
-                                onTap: () => print("SUCCSS"),
-                                child: null,
-                              ) as fmap.CircleMarker,
                               fmap.CircleMarker(
                                   point: lt.LatLng(item.values.elementAt(0),
                                       item.values.elementAt(1)),
@@ -80,6 +78,234 @@ class _MapCampaignRequestState extends State<MapCampaignRequest> {
                             ],
                           ),
                         ),
+                      for (var item in circleMarkersCampaigns)
+                        fmap.MarkerLayerWidget(
+                          options: fmap.MarkerLayerOptions(markers: [
+                            fmap.Marker(
+                                point: lt.LatLng(item.values.elementAt(0),
+                                    item.values.elementAt(1)),
+                                builder: (context) {
+                                  return StreamBuilder<DocumentSnapshot>(
+                                      stream: FirebaseFirestore.instance
+                                          .collection("admin_campaign_requests")
+                                          .doc(item.values.elementAt(3))
+                                          .snapshots(),
+                                      builder: (context, snapshot) {
+                                        if (!snapshot.hasData) {
+                                          return Center(
+                                            child: CircularProgressIndicator(),
+                                          );
+                                        } else {
+                                          return GestureDetector(
+                                            onTap: () => showDialog(
+                                                context: context,
+                                                builder: (context) {
+                                                  return Container(
+                                                    margin: EdgeInsets.fromLTRB(
+                                                        100, 100, 100, 100),
+                                                    child: Card(
+                                                      child: Column(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .center,
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .center,
+                                                        children: [
+                                                          Text(
+                                                              snapshot.data!.get(
+                                                                  'campaign_name'),
+                                                              style: const TextStyle(
+                                                                  color: Color(
+                                                                      0xff65BFB8),
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold,
+                                                                  fontSize:
+                                                                      30)),
+                                                          Text(snapshot.data!
+                                                              .get('address')),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'campaignID')),
+                                                          Text(snapshot.data!
+                                                              .get('city')),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'current_donations')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'current_volunteers')
+                                                              .toString()),
+                                                          Text(snapshot.data!.get(
+                                                              'date_created')),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'date_ended')),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'date_start')),
+                                                          Text(snapshot.data!.get(
+                                                              'description')),
+                                                          Text(snapshot.data!
+                                                              .get('latitude')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get('longitude')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'max_donation')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'number_of_seeds')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get(
+                                                                  'number_volunteers')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get('radius')
+                                                              .toString()),
+                                                          Text(snapshot.data!
+                                                              .get('time')),
+                                                          Text(snapshot.data!
+                                                              .get('uid')),
+                                                          Text(snapshot.data!
+                                                              .get('username')),
+                                                          SizedBox(
+                                                            height: 50,
+                                                          ),
+                                                          ElevatedButton(
+                                                              onPressed: () {
+                                                                context
+                                                                    .read(
+                                                                        authserviceProvider)
+                                                                    .createCampaign(
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'campaign_name'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'description'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'campaignID'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'date_created'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'date_start'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'date_ended'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'address'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'city'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'time'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'uid'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'username'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'latitude'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'longitude'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'number_of_seeds'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'current_donations'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'max_donation'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'current_volunteers'),
+                                                                        snapshot
+                                                                            .data!
+                                                                            .get(
+                                                                                'number_volunteers'))
+                                                                    .whenComplete(() =>
+                                                                        Navigator.pushNamed(
+                                                                            context,
+                                                                            '/map'));
+                                                              },
+                                                              child: Text(
+                                                                  "Approve This Campaign")),
+                                                          ElevatedButton(
+                                                              style: ElevatedButton
+                                                                  .styleFrom(
+                                                                      primary:
+                                                                          Colors
+                                                                              .red),
+                                                              onPressed: () {
+                                                                context
+                                                                    .read(
+                                                                        authserviceProvider)
+                                                                    .declineCampaign(
+                                                                        snapshot
+                                                                            .data!
+                                                                            .id)
+                                                                    .whenComplete(() =>
+                                                                        Navigator.pushNamed(
+                                                                            context,
+                                                                            '/new_map'));
+                                                              },
+                                                              child: Text(
+                                                                  "Decline This Campaign")),
+                                                          ElevatedButton(
+                                                              onPressed: () =>
+                                                                  Navigator.pop(
+                                                                      context),
+                                                              child:
+                                                                  Text("Back"))
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  );
+                                                }),
+                                            child: Icon(
+                                              Icons.circle_outlined,
+                                              size: item.values.elementAt(2) *
+                                                  100,
+                                            ),
+                                          );
+                                        }
+                                      });
+                                })
+                          ]),
+                        )
                     ]),
                 Row(
                   children: [
