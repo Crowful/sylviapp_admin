51ad7bd15f5c664c1f7ec5ad8280aab4d60d5254
softfietea <tribaco.alfie@gmail.com>
2021-12-01 00:52:12 +0800

    update report part 2

--- lib/campaign_reports.dart
+++ lib/campaign_reports.dart
@@ -27,77 +27,101 @@ class _CampaignReportsState extends State<CampaignReports> {
                 return ListView(
                   children: snapshot.data!.docs.map((e) {
                     return Container(
-                      width: 100,
-                      height: 200,
                       margin: EdgeInsets.fromLTRB(500, 0, 500, 10),
-                      child: Card(
-                        child: Container(
-                            margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                            child: Row(children: [
-                              Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      e.get('campaign_name'),
-                                      style: TextStyle(
-                                          color: Color(0xff65BFB8),
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(
-                                      'Scam Report: ' +
-                                          e.get('reportScam').toString(),
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w300),
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(
-                                      'Abuse Report: ' +
-                                          e.get('reportAbuse').toString(),
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w300),
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(
-                                      'Inappropriate Words: ' +
-                                          e.get('reportUIW').toString(),
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w300),
-                                    )
-                                  ]),
-                              SizedBox(
-                                width: 200,
-                              ),
-                              Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    ElevatedButton(
-                                        onPressed: () {},
-                                        child: Text('delete campaign')),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    ElevatedButton(
-                                        onPressed: () {},
-                                        child: Text('info of organizer')),
-                                  ],
+                      child: Row(children: [
+                        Container(
+                          width: 500,
+                          height: 200,
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Card(
+                            child: Container(
+                                margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                child: Row(children: [
+                                  Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          e.get('campaign_name'),
+                                          style: TextStyle(
+                                              color: Color(0xff65BFB8),
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          'Scam Report: ' +
+                                              e.get('reportScam').toString(),
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w300),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          'Abuse Report: ' +
+                                              e.get('reportAbuse').toString(),
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w300),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          'Inappropriate Words: ' +
+                                              e.get('reportUIW').toString(),
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w300),
+                                        )
+                                      ]),
+                                ])),
+                          ),
+                        ),
+                        Container(
+                          width: 210,
+                          height: 200,
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Card(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 200,
+                                  width: 100,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.red),
+                                      onPressed: () {},
+                                      child: Text('delete campaign')),
                                 ),
-                              )
-                            ])),
-                      ),
+                                Container(
+                                  height: 200,
+                                  width: 100,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.blue),
+                                      onPressed: () {
+                                        showDialog(
+                                            context: context,
+                                            builder: (context) {
+                                              return Card(child: Text('Test'));
+                                            });
+                                      },
+                                      child: Text('info of organizer')),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ]),
                     );
                   }).toList(),
                 );
